ea6270ade92bb35ddaa5d8e926015c69e6940bdc
Marcos Vinícius <marcosviniciusdacunharocha6@gmail.com>
2024-12-20 14:00:53 -0300

    Add files via upload

---
+++ footer.components.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  imports: [RouterLink],
+  templateUrl: './footer.components.html',
+  styleUrl: './footer.components.css'
+})
+export class FooterComponent {
+
+}
